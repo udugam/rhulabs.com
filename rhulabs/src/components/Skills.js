23d150efea2733269ba287c188d5ce37280ad015
udugam <ryan.udugam@gmail.com>
2019-08-31 21:31:37 -0400

    added more css and content to skills section

--- rhulabs/src/components/Skills.js
+++ rhulabs/src/components/Skills.js
@@ -7,7 +7,12 @@ const Skills = (props) => {
         },
         listBox: {
             width: '66vw',
-            alignSelf: 'center'
+            alignSelf: 'center',
+            paddingTop: '50px'
+        },
+        summary: {
+            paddingTop: '10px',
+            paddingBottom: '10px'
         }
     
     }
@@ -15,20 +20,28 @@ const Skills = (props) => {
     return(
         <div style={styles.container}>
             <div style={styles.listBox}>
-                <img alt='Front End Icon' src='https://via.placeholder.com/150'/>
+                <img alt='Front End Icon' src='https://via.placeholder.com/50'/>
                 <h1>Frontend Developer</h1>
-                <div>Node.js</div>
-                <div>Node.js</div>
+                <div style={styles.summary}>I value simple content structure, clean design patterns, and thoughtful interactions.</div>
+                <div>React</div>
+                <div>HTML</div>
+                <div>CSS</div>
+                <div>jQuery</div>
+                <div>Javascript</div>
             </div> 
             <div style={styles.listBox}>
-                <img alt='Back End Icon' src='https://via.placeholder.com/150'/>
+                <img alt='Back End Icon' src='https://via.placeholder.com/50'/>
                 <h1>Backend Developer</h1>
-                <div>Node.js</div>
-                <div>Node.js</div>
+                <div style={styles.summary}>I like to code things from scratch, and enjoy bringing ideas to life in the browser.</div>
+                <div>Node</div>
+                <div>MongoDB</div>
+                <div>mySQL</div>
+                <div>Serverless Lambda Functions</div>
             </div>        
             <div style={styles.listBox}>
-                <img alt='Back End Icon' src='https://via.placeholder.com/150'/>
+                <img alt='Back End Icon' src='https://via.placeholder.com/50'/>
                 <h1>Mentor</h1>
+                <div style={styles.summary}>I genuinely care about people, and love helping fellow designers work on their craft.</div>
                 <div>Node.js</div>
                 <div>Node.js</div>
             </div>       
